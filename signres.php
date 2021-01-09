7fcb2cc7a8528e97e40aa7659e47941a6cdd10bb
Umme Kawser Sinthia <40598386+sinthy08@users.noreply.github.com>
2021-01-09 13:54:57 +0600

    Result of signup page

---
+++ signres.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>480 project</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+<h1>Solvers Den</h1>
+
+<div class="topnav" align="center">
+  <a class="active" href="index.php">Home</a>
+  <a href="post.php">Posts</a>
+  <a href="create.php">Create Post</a>
+  <a href="contacts.php">Contact</a>
+  <a href="about.php" style="margin-right: 180px">About</a>
+
+
+  <button id="logout" onclick="location.href = 'index.php';">Logout</button>
+
+</div> 
+</head>
+
+<body style="background-image: url(red.jpg);">
+
+<img src="home.jpg" height="300px" weight="300px" style="padding-left: 100px; padding-right: 100px;">
+<footer style="color: white; text-align: center; background-color: black; padding: 0.5px; margin-top: 70px;">
+	<p> Copyright © 2018 Solvers den. All rights reserved</p></footer>
+
+</body>
+</html>
